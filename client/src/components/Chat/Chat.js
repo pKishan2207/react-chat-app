ee055da2b6835d6309c183b6e8c5b767607240d7
Kishankumar patel <kishankumar.patel@ip-192-168-0-104.ap-south-1.compute.internal>
2021-04-20 19:34:29 +0530

    heroku config added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState([]);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://react-chat-app-kishan.herokuapp.com/";
 
   useEffect(() => {
     const connectionOptions = {
